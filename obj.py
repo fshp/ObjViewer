bd9637f085506937339d9f989e20bfbaa9e55112
Franchesko Salias Hudro Pedros <franchesko.salias.hudro.pedros@gmail.com>
2015-02-01 22:33:45 +0300

    Model viewer

--- obj.py
+++ obj.py
@@ -1,3 +1,5 @@
+import numpy
+
 class OBJ:
     def __init__(self, filename):
         self.vertices = []
@@ -9,7 +11,19 @@ class OBJ:
             if not values:
                 continue
             if values[0] == 'v':
-                v = tuple(map(float, values[1:4]))
-                self.vertices.append(v)
+                v = list(map(float, values[1:4]))
+                a, b, c = v
+                self.vertices.append([a, b, c])
             elif values[0] == 'f':
-                self.faces.append(tuple(map(lambda x: int(x), values[1:4])))
+                for x in values[1:4]:
+                    self.faces += [int(x.split('/')[0]) - 1, ]
+
+        # tmp = []
+        # for i in self.faces:
+        # tmp += self.vertices[i]
+
+        self.vertices = numpy.array(self.vertices, dtype=numpy.float32)
+        self.faces = numpy.array(self.faces, dtype=numpy.uint32)
+
+    def getArray(self):
+        return list([[self.vertices[a - 1], self.vertices[b - 1], self.vertices[c - 1]] for a, b, c in self.faces])
--- prog.py
+++ prog.py
@@ -1,182 +1,95 @@
-#!/usr/bin/python
-
+# -*- coding: utf-8 -*-
 from OpenGL.GL import *
-from OpenGL.GLU import *
 from OpenGL.GLUT import *
-from kg import *
-from obj import *
+
+from obj import OBJ
 
 
-def InitGL(Width, Height):
-    glClearColor(0.0, 0.0, 0.0, 0.0)
-    glClearDepth(1.0)
-    glDepthFunc(GL_LESS)
-    glEnable(GL_DEPTH_TEST)
-    # glEnable(GL_LIGHTING)
-    #glEnable(GL_LIGHT0)
-    #glEnable(GL_COLOR_MATERIAL)
-    glMatrixMode(GL_PROJECTION)
-    glLoadIdentity()
-    gluPerspective(45.0, float(Width) / float(Height), 0.1, 1000.0)
-    glMatrixMode(GL_MODELVIEW)
+def keypress(key, x, y):
+    if key == b'w':
+        glRotatef(5, 1, 0, 0)
+    elif key == b's':
+        glRotatef(-5, 1, 0, 0)
+    elif key == b'a':
+        glRotatef(5, 0, 1, 0)
+    elif key == b'd':
+        glRotatef(-5, 0, 1, 0)
+    elif key == b'q':
+        glRotatef(5, 0, 0, 1)
+    if key == b'e':
+        glRotatef(-5, 0, 0, 1)
 
 
-def DrawGrid(length, a):
-    i = 0
-    global s
-    # if a<=0: s=a=0.1
-
-    glColor3f(1, 1, 1)
-    glBegin(GL_LINES)
-    glVertex2f(-length, 0)
-    glVertex2f(length, 0)
-    glVertex2f(0, -length)
-    glVertex2f(0, length)
-    glEnd()
-
-    glColor3f(0.2, 0.2, 0.2)
-    while i <= length:
-        glBegin(GL_LINES)
-        glVertex2f(-length, i)
-        glVertex2f(length, i)
-        glVertex2f(-length, -i)
-        glVertex2f(length, -i)
-        glVertex2f(i, -length)
-        glVertex2f(i, length)
-        glVertex2f(-i, -length)
-        glVertex2f(-i, length)
-        glEnd()
-        i += a
+def specialkeys(key, x, y):
+    if key == GLUT_KEY_UP:  # Клавиша вверх
+        glRotatef(5, 1, 0, 0)  # Вращаем на 5 градусов по оси X
+    if key == GLUT_KEY_DOWN:  # Клавиша вниз
+        glRotatef(-5, 1, 0, 0)  # Вращаем на -5 градусов по оси X
+    if key == GLUT_KEY_LEFT:  # Клавиша влево
+        glRotatef(5, 0, 1, 0)  # Вращаем на 5 градусов по оси Y
+    if key == GLUT_KEY_RIGHT:  # Клавиша вправо
+        glRotatef(-5, 0, 1, 0)  # Вращаем на -5 градусов по оси Y
 
 
-def ReSizeGLScene(Width, Height):
-    if Height == 0: Height = 1
-    glViewport(0, 0, Width, Height)
-    glMatrixMode(GL_PROJECTION)
-    glLoadIdentity()
-    gluPerspective(45.0, float(Width) / float(Height), 0.1, 100.0)
-    glMatrixMode(GL_MODELVIEW)
+def create_shader(shader_type, source):
+    shader = glCreateShader(shader_type)
+    glShaderSource(shader, source)
+    glCompileShader(shader)
+    return shader
 
 
-def DrawGLScene():
+def draw():
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
-    glLoadIdentity()
-    global x, y, z
-    glTranslatef(x, y, z)
-    global q
-    modem = [GL_FILL, GL_LINE, GL_POINT]
-    colorm = ((0.5, 0.5, 0.5), (1, 0, 0), (0, 0, 1))
+    glEnable(GL_PROGRAM_POINT_SIZE)
 
-    for mode, color in zip(modem, colorm):
-        glPolygonMode(GL_FRONT_AND_BACK, mode)
-        glBegin(GL_QUADS)
-        r, g, b = color
-        glColor(r, g, b)
-        for i in q():
-            i.pop()
-            glVertex3fv(i)
-        glEnd()
+    glEnableClientState(GL_VERTEX_ARRAY)
+    glVertexPointer(3, GL_FLOAT, 0, pointdata)
 
-    global l
-    global s
-    DrawGrid(l, s)
+    glPolygonMode(GL_FRONT_AND_BACK, GL_POINT)
+    glColor(0, 0, 1)
+    glDrawElements(GL_TRIANGLES, len(indexes), GL_UNSIGNED_INT, indexes)
 
+    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE)
+    glColor(1, 0, 0)
+    glDrawElements(GL_TRIANGLES, len(indexes), GL_UNSIGNED_INT, indexes)
+
+    glPolygonMode(GL_FRONT_AND_BACK, GL_FILL)
+    glColor(1, 1, 1)
+    glDrawElements(GL_TRIANGLES, len(indexes), GL_UNSIGNED_INT, indexes)
     glutSwapBuffers()
 
 
-def KeyPressed(*args):
-    global q
-    global s
-    global l, x, y, z
-    if args[0] == "\033":
-        sys.exit()
-    elif args[0] == b"w":
-        q.set_z(-1)
-    elif args[0] == b"s":
-        q.set_z(1)
-    elif args[0] == b"a":
-        q.set_x(-1)
-    elif args[0] == b"d":
-        q.set_x(1)
-    elif args[0] == b"q":
-        q.set_y(-1)
-    elif args[0] == b"e":
-        q.set_y(1)
-    elif args[0] == b"u":
-        q.set_anglez(2)
-    elif args[0] == b"j":
-        q.set_anglez(-2)
-    elif args[0] == b"h":
-        q.set_anglex(2)
-    elif args[0] == b"k":
-        q.set_anglex(-2)
-    elif args[0] == b"y":
-        q.set_angley(2)
-    elif args[0] == b"i":
-        q.set_angley(-2)
+glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB)
+glutInitWindowSize(900, 900)
+glutInitWindowPosition(50, 50)
+glutInit(sys.argv)
+glutCreateWindow(b"Py3D")
+glutDisplayFunc(draw)
+glutIdleFunc(draw)
+glutSpecialFunc(specialkeys)
+glutKeyboardFunc(keypress)
+glClearColor(0.2, 0.2, 0.2, 1)
+vertex = create_shader(GL_VERTEX_SHADER, """
+varying vec4 vertex_color;
+            void main(){
+                gl_Vertex.xyz *=  1.0f;
+                gl_Position = gl_ModelViewProjectionMatrix * gl_Vertex;
+                vertex_color = gl_Color;
+                gl_PointSize = 3.0;
+            }""")
+fragment = create_shader(GL_FRAGMENT_SHADER, """
+varying vec4 vertex_color;
+            void main() {
+                gl_FragColor = vertex_color;
 
-
-def SpecialKeyPressed(*args):
-    tr = 1
-    if args[0] == GLUT_KEY_UP:
-        q.set_y(tr)
-    elif args[0] == GLUT_KEY_DOWN:
-        q.set_y(-tr)
-    elif args[0] == GLUT_KEY_RIGHT:
-        q.set_x(tr)
-    elif args[0] == GLUT_KEY_LEFT:
-        q.set_x(-tr)
-
-
-def main():
-    glutInit(sys.argv)
-    glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_ALPHA | GLUT_DEPTH)
-    glutInitWindowSize(400, 300)
-    glutInitWindowPosition(0, 0)
-    glutCreateWindow("2d")
-    glutDisplayFunc(DrawGLScene)
-    glutIdleFunc(DrawGLScene)
-    glutReshapeFunc(ReSizeGLScene)
-    glutKeyboardFunc(KeyPressed)
-    glutSpecialFunc(SpecialKeyPressed)
-    #glutFullScreen()
-    InitGL(400, 300)
-    glutMainLoop()
-
-
-x = 0
-y = 0
-z = -20
-a = 3.0
-l = 10
-s = 1
-# q = Polygons3f([Point3f(-a,-a,-a)(), Point3f(a,-a,-a)(), Point3f(-a,a,a)()])#, Point(a,a)()])
-q = Polygons3f([Point3f(-a, -a, -a)(),
-                Point3f(-a, a, -a)(),
-                Point3f(-a, a, a)(),
-                Point3f(-a, -a, a)(),
-                Point3f(a, -a, -a)(),
-                Point3f(a, -a, a)(),
-                Point3f(a, a, a)(),
-                Point3f(a, a, -a)(),
-                Point3f(-a, -a, -a)(),
-                Point3f(-a, -a, a)(),
-                Point3f(a, -a, a)(),
-                Point3f(a, -a, -a)(),
-                Point3f(-a, a, -a)(),
-                Point3f(-a, a, a)(),
-                Point3f(a, a, a)(),
-                Point3f(a, a, -a)(),
-                Point3f(-a, -a, -a)(),
-                Point3f(a, -a, -a)(),
-                Point3f(a, a, -a)(),
-                Point3f(-a, a, -a)(),
-                Point3f(-a, -a, a)(),
-                Point3f(a, -a, a)(),
-                Point3f(a, a, a)(),
-                Point3f(-a, a, a)()
-])
-
-model = OBJ("model.obj")
-
-main()
+}""")
+program = glCreateProgram()
+glAttachShader(program, vertex)
+glAttachShader(program, fragment)
+glLinkProgram(program)
+glUseProgram(program)
+model = OBJ('auto.obj')
+pointdata = model.vertices
+indexes = model.faces
+glEnable(GL_DEPTH_TEST)
+glutMainLoop()
